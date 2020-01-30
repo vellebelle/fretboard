69ebf9d51edb4b41331319fb852e88d091870779
Morten Vestergaard <vellebelle@gmail.com>
2020-01-31 01:00:18 +0700

    Added single fretmarks dynamically

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const fretboard = document.querySelector('.fretboard');
 // testing
+// WAIT WITH THESE VARIABLES UNTIL BEING USED. OTHERWISE CONFUSING..
 // Setup consts and variables
 const numberOfFrets = 12;
 const numberOfStrings = 6;
-const tuning = ['E', 'B', 'G', 'D', 'A', 'E']; // reverse order from top of screen
+
+
+//WAIT WITH TUNING
+// const tuning = ['E', 'B', 'G', 'D', 'A', 'E']; // reverse order from top of screen
 
 //WAIT WITH THIS.. CONFUSING IF TOO EARLY
 // sharps the noteletter without the accidental. ie: C, C#, D, D#, E, F, F#, G, G#, A, A#, B
-const notesSharp = ["C", "C", "D", "D", "E", "F", "F", "G", "G", "A", "A", "B"];
+// const notesSharp = ["C", "C", "D", "D", "E", "F", "F", "G", "G", "A", "A", "B"];
 // Same concept for the flats
-const notesFlat = ["C", "D", "D", "E", "E", "F", "G", "G", "A", "A", "B", "B"];
+// const notesFlat = ["C", "D", "D", "E", "E", "F", "G", "G", "A", "A", "B", "B"];
 
 // determines whether the note dots should use flats or sharps
-let accidentals = 'flats';
+// let accidentals = 'flats';
+
+// Where the single fretmarks should be positioned on the fretboard
+const singleFretMarkPositions = [3, 5, 7, 9];
 
 const app = {
     init() {
@@ -31,6 +38,12 @@ const app = {
                 // >= operator because we have 1+ fret because of 0ith fret
                 let noteFret = tools.createElement('div');
                 noteFret.classList.add('note-fret');
+                // Add single fretmarks to specific frets, but only on the first string (i === 0)
+                // AND Check to see if we are in a fret, where a single fretmark should be added
+                if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
+                    noteFret.classList.add('single-fretmark');
+                }
+                
                 string.appendChild(noteFret);
             }
         }
